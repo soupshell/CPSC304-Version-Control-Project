9a68f832d58f8010878227f57ecf9f628edbd21b
charity-g <charity.grey1@gmail.com>
2024-07-26 16:17:08 -0700

    UsersList blocked out

--- frontend/src/pages/UsersList.jsx
+++ frontend/src/pages/UsersList.jsx
@@ -3,7 +3,19 @@ function UsersList(props) {
 
    return (
      <>
-      <p> Users List</p>
+       <h1>Our Users</h1>
+       <div className="centerDiv">
+        <p>Projection</p>
+        <p>Aggregation Nested</p>
+        <p>Aggregation HAVING</p>
+       </div>
+
+       <div className="centerDiv">
+        <div>
+          <p>Select Repos</p>
+          <p>Results</p>
+        </div> 
+       </div>
      </>
    )
  }
