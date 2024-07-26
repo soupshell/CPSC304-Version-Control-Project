8d5720993f854e40da2990212e5439cf9ff63457
charity-g <charity.grey1@gmail.com>
2024-07-26 15:47:00 -0700

    Added react-router-dom

--- frontend/src/components/AuthBox.jsx
+++ frontend/src/components/AuthBox.jsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 
 function AuthBox(props) {
    const title = props.title;
-   const description = 'this is login description';
-   const buttonText = 'no link';
-   const link = 'no';
-
-
+   const description = (title && title == 'Login') ? "For users with existing accounts" : "For first time users!";
+   const handleSubmit = () => {
+      console.log(title + " submitted.")
+   };
+   const [formValues, setFormValues] = useState({});
+   const handleChange = (e) => {
+    setFormValues({[e.target.id]:e.target.value});
+    console.log(formValues); 
+   }
+   
    return (
    <div className="card-container">      
       {title && <h1 >{title}</h1>}
-      {description && <p >{description}</p>}
-      {buttonText && link && (
-        <p>
-          {buttonText}
-        </p>
-      )}
+      <p >{description}</p>
+      <form onSubmit={handleSubmit}>
+      <label> Email:
+        <input type="text" onChange={handleChange} />        
+      </label>
+      <label> Password:
+        <input type="password" onChange={handleChange} />        
+      </label>
+      {title != 'Login' && 
+          <label> Username:
+            <input type="text" onChange={handleChange} />        
+          </label>}
+      <input type="submit" value="Submit" />
+      </form>
     </div>
    )
  }
