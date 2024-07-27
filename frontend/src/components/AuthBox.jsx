b1dc379b00d0673fd5065ff2985d34c1dfc6a334
charity-g <charity.grey1@gmail.com>
2024-07-26 20:38:41 -0700

    Login authentication system working locally

--- frontend/src/components/AuthBox.jsx
+++ frontend/src/components/AuthBox.jsx
@@ -4,15 +4,13 @@ function AuthBox(props) {
    const title = props.title;
    const description = (title && title == 'Login') ? "For users with existing accounts" : "For first time users!";
    const handleSubmit = () => {
-      console.log(title + " submitted.")
-      console.log()
+      props.verifyFn(formValues);
    };
    const [formValues, setFormValues] = useState({});
    const handleChange = (e) => {
     let temp = formValues
     temp[e.target.id] = e.target.value
     setFormValues(formValues);
-    console.log(formValues); 
    }
    
    return (
@@ -21,14 +19,14 @@ function AuthBox(props) {
       <p >{description}</p>
       <form onSubmit={handleSubmit}>
       <label> Email:
-        <input type="text" id={title +'-email'} onChange={handleChange} required/>        
+        <input type="email" id={'email'} onChange={handleChange} required/>        
       </label>
       <label> Password:
-        <input type="password" id={title +'-pwd'} onChange={handleChange} required/>        
+        <input type="password" id={'pwd'} onChange={handleChange} required/>        
       </label>
       {title != 'Login' && 
           <label> Username:
-            <input type="text" id={title +'-username'} onChange={handleChange} required/>        
+            <input type="text" id={'username'} onChange={handleChange} required/>        
           </label>}
       <input type="submit" value="Submit" />
       </form>
