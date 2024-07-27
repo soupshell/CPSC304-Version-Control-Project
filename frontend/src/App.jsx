97a6f7ec21a7d6b272a86bb7de1b15a8832ab2b2
marcusg9 <marcusg9@students.cs.ubc.ca>
2024-07-26 21:13:28 -0700

    updated pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Home from './pages/Home';
 import RepoHome from './pages/RepoHome';
+import FilePage from './pages/Files'
 import './App.css';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <Routes>
             <Route path="/" element={<Login />}/>
             <Route path="/Home" element={<Home />}/>
-            <Route path="/Repo" element={<RepoHome />}/>
+            <Route path="/Repo" element={<RepoHome />}  />
+            <Route path="/:user/:repoName/:filename" element={<FilePage />}/>      
       </Routes>
       </div>
       </Router>
---
+++ frontend/src/pages/Files.jsx
@@ -0,0 +1,17 @@
+
+
+
+function FilePage(props){
+  const repoName = "hello";
+  const currentUserId = 1;
+
+  // Check if user can access; 
+
+    return (
+        <>
+         <p> File Page</p>
+        </>
+      );
+}
+
+export default FilePage;
