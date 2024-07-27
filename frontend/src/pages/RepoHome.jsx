b9abb27de080a83cff11b1c3d3bb9d65b94c6cea
charity-g <charity.grey1@gmail.com>
2024-07-26 21:52:20 -0700

    RepoHome paragraphed major tags

--- frontend/src/pages/RepoHome.jsx
+++ frontend/src/pages/RepoHome.jsx
@@ -1,27 +1,61 @@
 import RepoHeader from "../components/RepoHeader";
-import {useParams, Link} from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
 
 function RepoHome(props) {
-  const params = useParams();
-  const max_repo_id = 2;
-  
-  if (params.id > max_repo_id) {
+  const params = useParams(); // access params.id
+  //TODO THIS IS THE EXAMPLE REPODATA- should be async query in live
+  const repoData = {
+    id: 1,
+    repoName: "testRepoName",
+    currBranchName: "branchName",
+    currCommitId: "123",
+    currFolderPath: "/",
+    folders: [{ filepath: "/folder1", fileId: "12" }],
+    files: [
+      { filepath: "/file1.txt", fileId: "3" },
+      { filepath: "/file2.cpp", fileId: "3" },
+    ],
+    contributors: [{ username: "Edit", username2: "Owner" }],
+  };
+  const [repoState, setState] = useState(repoData);
+  if (!repoData) {
     return (
       <>
-      <h1>404 error: You are in a repo that you should not have access to/doesn't exist.</h1>
-      <Link to='/' className='ctgrey-button'> Return to home screen. </Link>
+        <h1>
+          404 error: You are in a repo that you should not have access
+          to/doesn't exist.
+        </h1>
+        <Link to="/" className="ctgrey-button">
+          {" "}
+          Return to home screen.{" "}
+        </Link>
       </>
     );
   }
 
-   return (
-     <>
-      <RepoHeader/>
-      <p> Repo Home Page</p>
-      <p>{params.id}</p>
-     </>
-   );
- }
- 
- export default RepoHome;
- 
+  return (
+    <>
+      <RepoHeader />
+      <div className="centerDiv">
+        <h2>
+          Repo <i>{repoState["repoName"]}</i>'s Home Page
+        </h2>
+        <h3>current branch: {repoState["currBranchName"]}</h3>
+      </div>
+      <div className="centerDiv">
+        <h3> {repoState["currFolderPath"]} </h3>
+        <h3> commitId: {repoState['currCommitId']}</h3>
+      </div>
+      <div className="centerDiv">
+        <p>Folders and files</p>
+        <div>
+          <p>Contributors</p>
+          <p>issues Button</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RepoHome;
